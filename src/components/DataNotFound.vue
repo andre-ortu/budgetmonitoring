41f63e42022288c1caa4899386ae77dcd7387c0b
sc-andreaortu <andrea.ortu@smart-contact.it>
2021-09-27 09:38:14 +0200

    DataNotFound

---
+++ src/components/DataNotFound.vue
@@ -0,0 +1,18 @@
+<template>
+    <el-empty :description="description"></el-empty>
+</template>
+
+<script>
+export default {
+    name: "DataNotFound",
+    props: {
+        description: {
+            default: 'Nessun Dato Disponibile'
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
